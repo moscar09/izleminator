02895f2129418250f5020754a078aae49cab2cd1
Oscar Marginean <oscar.marginean@best.eu.org>
2017-11-12 14:24:34 +0100

    Tied into the netflix API

--- chrome-extension/scripts/playerWrappers/cadmium-player-wrapper.js
+++ chrome-extension/scripts/playerWrappers/cadmium-player-wrapper.js
@@ -1,8 +1,12 @@
 'use strict';
 window.CadmiumPlayerWrapper = class {
     constructor(communicator) {
-        this.videoElement =  document.getElementsByTagName('video')[0];
+        this.videoElement = document.getElementsByTagName('video')[0];
         this.communicator = communicator;
+
+        var videoPlayer     = netflix.appContext.state.playerApp.getAPI().videoPlayer;
+        var playerSessionId = videoPlayer.getAllPlayerSessionIds()[0];
+        this.videoPlayer    = videoPlayer.getVideoPlayerBySessionId(playerSessionId);
 
         var self = this;
 
@@ -15,32 +19,23 @@ window.CadmiumPlayerWrapper = class {
         this.videoElement.addEventListener("pause", function(e) {
             self.communicator.postMessage('pausePlayer');            
         })
-
     }
 
-    static get playerMedia() {
-        return $('.NFPlayer');
-    }
-
-    static get playerWrapper() {
-        return $('.NFPlayer');
-    }
-
-    static get includeFilename() {
-        return "cadmium-player-wrapper.js";
-    }
-
-    static isContextReady() {
-        return this.playerWrapper.length == 1;
-    }
+    static get playerMedia()     { return $('.NFPlayer'); }
+    static get playerWrapper()   { return $('.NFPlayer'); }
+    static get includeFilename() { return "cadmium-player-wrapper.js"; }
+    static isContextReady()      { return this.playerWrapper.length == 1; }
 
     pause() {
+        this.videoPlayer.pause();
     }
 
     seek(position) {
+        this.videoPlayer.seek(position);
     }
 
     start() {
+        this.videoPlayer.play();
     }
 
 }
