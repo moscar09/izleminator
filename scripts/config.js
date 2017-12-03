5b0c340bb66a6d40a1227d3199201b486a2b424f
Oscar Marginean <oscar.marginean@gmail.com>
2017-12-03 23:12:17 +0100

    Moved the server code outside of this repo

---
+++ scripts/config.js
@@ -0,0 +1,3 @@
+var key = 'gwcx2kDNlCYNdd3IOAzbYreSlj8ZaJRUuEK483J0Vq6eNW98ct54qoaUVuRk9LFoYUqLCN95oWrNNo9L9mT0ELKGrdxtgSAyledr';
+// var socketUri = 'wss://moscar.ro:8080/izleminator-server/chat';
+var socketUri = 'ws://localhost:8080/izleminator-server/chat';
